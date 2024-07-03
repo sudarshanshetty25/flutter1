f4a48db593813ab0cfc59d61ca1c5691d581f9f4
Sudarshan Shetty <sudarshanshetty2563.com>
2024-07-03 18:22:33 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:demo/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: gardientcontainer(),
+        
       ),
     ),
   );
